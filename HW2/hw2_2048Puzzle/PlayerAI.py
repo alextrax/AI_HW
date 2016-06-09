9fc0c365a49ef2923c8fdbc711200869037079b3
alextrax <cw2952@columbia.edu>
2016-06-08 23:06:17 -0400

    Add mix heuristic function and time limit

--- HW2/hw2_2048Puzzle/PlayerAI.py
+++ HW2/hw2_2048Puzzle/PlayerAI.py
@@ -3,10 +3,14 @@
 
 from random import randint
 from BaseAI import BaseAI
+import math
+import time
 
 #vecIndex = [UP, DOWN, LEFT, RIGHT] 
 DEFAULT_DEPTH = 8
 MAX_VALUE = 2**32 - 1
+TIME_LIMIT = 0.4
+
 class PlayerAI(BaseAI):
     def MaxTile(self, grid):
         return grid.getMaxTile()
@@ -14,12 +18,41 @@ class PlayerAI(BaseAI):
     def EmptyTile(self, grid):
         return len(grid.getAvailableCells())   
 
+    def SameValue(self, grid):
+        count = 0
+        for i in xrange(grid.size - 1):
+            for j in xrange(grid.size - 1):
+                if grid.map[i][j] == grid.map[i][j+1]:
+                    count += 1
+                if grid.map[i][j] == grid.map[i+1][j]:
+                    count += 1
+        return count
+
+    def MixHeuristic(self, grid):
+        empty_weight = 1.0
+        max_weight = 1.0
+        same_weight = 1.0
+        empty_count =  len(grid.getAvailableCells())
+        max_tile = grid.getMaxTile()
+        same_count = self.SameValue(grid)
+        return empty_count + max_tile + same_count
+        #return len(grid.getAvailableCells()) + \
+        #       math.log(grid.getMaxTile(), 2) + \
+        #       self.SameValue(grid) 
+
     def getMove(self, grid):
-        d, v = self.search(grid, DEFAULT_DEPTH, -1, MAX_VALUE, True, self.EmptyTile)
+        #moves = grid.getAvailableMoves()
+        #return moves[randint(0,len(moves)-1)]
+        start = time.time()
+        depth = 0
+        while (time.time() - start) < TIME_LIMIT:
+            depth += 1
+            d, v = self.search(grid, depth, -1, MAX_VALUE, True, self.MixHeuristic)
+        print depth  
+        print time.time() - start  
         return d
 
     def search(self, grid, depth, alpha, beta, isMAX, heuristic):
-
         moves = grid.getAvailableMoves()
         if depth == 0 or moves == None:
             return 4, heuristic(grid)	
