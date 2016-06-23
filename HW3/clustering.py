2e1a25c9ce863aa5dd5501dbf546de19dba343c9
alextrax <cw2952@columbia.edu>
2016-06-23 14:50:34 -0400

    Clustering code clean

--- HW3/clustering.py
+++ HW3/clustering.py
@@ -15,26 +15,17 @@ trees = np.array(trees, dtype=np.float64) / 255
 
 w, h, d = tuple(trees.shape)
 trees_2d = np.reshape(trees, (w * h, d))
-trees_rdm = shuffle(trees_2d, random_state=0)[:1000]
-kmeans = KMeans(n_clusters=ncolors, random_state=0).fit(trees_rdm)
-labels = kmeans.predict(trees_2d)
-#print trees.shape
+kmeans = KMeans(n_clusters=ncolors, random_state=0).fit(trees_2d)
+quantized = kmeans.predict(trees_2d)
+ 
 
-def recreate_image(codebook, labels, w, h):
-    """Recreate the (compressed) image from the code book & labels"""
-    d = codebook.shape[1]
-    image = np.zeros((w, h, d))
-    label_idx = 0
-    for i in range(w):
-        for j in range(h):
-            image[i][j] = codebook[labels[label_idx]]
-            label_idx += 1
-    return image
+d = kmeans.cluster_centers_.shape[1]
+image = np.zeros((w, h, d))
+index = 0
+for i in range(w):
+    for j in range(h):
+        image[i][j] = kmeans.cluster_centers_[quantized[index]]
+        index += 1
 
-plt.figure(1)
-plt.clf()
-ax = plt.axes([0, 0, 1, 1])
-plt.axis('off')
-plt.title('Quantized image ( %d colors, K-Means)'%ncolors)
-plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))    
+plt.imshow(image)    
 plt.show()
