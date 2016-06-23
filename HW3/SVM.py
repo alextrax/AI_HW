b97d05a4d72b085945b6187fd8f82513c03077a4
alextrax <cw2952@columbia.edu>
2016-06-23 14:50:15 -0400

    SVM code refine

--- HW3/SVM.py
+++ HW3/SVM.py
@@ -32,14 +32,6 @@ train = np.genfromtxt ('train.csv', delimiter=",")
 label = train[:,2:3]
 label = np.reshape(label, label.shape[0])
 
-
-def accuracy(predict, label):
-	hit = 0.0
-	for i in range(len(predict)):
-		if predict[i] == label[i]:
-			hit += 1
-	return hit/len(predict)	
-
 def plotting(num, method, title):	
     plt.subplot(2, 3, num)
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
@@ -57,11 +49,9 @@ def plotting(num, method, title):
 
 
 
-linear_svc = svm.SVC(kernel='linear')
+linear_svc = svm.SVC(kernel='linear', C=1)
 linear_svc.fit(train[:,0:2], label)
-#result = linear_svc.predict(test[:,0:2])
-#acc = accuracy(result, np.reshape(test[:,2:3], test[:,2:3].shape[0]))
-#print "Linear kernel accuracy: %.2f%%" % (linear_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
+print "Linear kernel test data accuracy: %.2f%%" % (linear_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
 scores = cross_validation.cross_val_score(linear_svc, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
 print "Linear kernel Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
 plotting(2, linear_svc, "SVM - linear kernel")
@@ -69,28 +59,24 @@ plotting(2, linear_svc, "SVM - linear kernel")
 
 rbf_svc = svm.SVC(kernel='rbf')
 rbf_svc.fit(train[:,0:2], label)
-#result = rbf_svc.predict(test[:,0:2])
-#acc = accuracy(result, np.reshape(test[:,2:3], test[:,2:3].shape[0]))
-#print "RBF kernel accuracy: %.2f%%" % (rbf_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0])) *100)
+print "RBF kernel test data accuracy: %.2f%%" % (rbf_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0])) *100)
 scores = cross_validation.cross_val_score(rbf_svc, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
 print "RBF kernel Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
 plotting(3, rbf_svc, "SVM - RBF kernel")
 
 
 
-poly_svc = svm.SVC(kernel='poly')
+poly_svc = svm.SVC(kernel='poly', degree=7)
 poly_svc.fit(train[:,0:2], label)
-#result = poly_svc.predict(test[:,0:2])
-#acc = accuracy(result, np.reshape(test[:,2:3], test[:,2:3].shape[0]))
-#print "Polynomial kernel accuracy: %.2f%%" % (poly_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
-scores = cross_validation.cross_val_score(poly_svc, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
-print "Polynomial kernel Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
-plotting(4, poly_svc, "SVM - Polynomial kernel")
+print "Polynomial kernel test data accuracy: %.2f%%" % (poly_svc.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
+#scores = cross_validation.cross_val_score(poly_svc, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
+#print "Polynomial kernel Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+plotting(4, poly_svc, "SVM - Polynomial kernel\ndegree=7")
 
 
-
-logreg = linear_model.LogisticRegression(C=1e5)
+logreg = linear_model.LogisticRegression()
 logreg.fit(train[:,0:2], label)
+print "Logistic Regression test data accuracy: %.2f%%" % (logreg.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
 scores = cross_validation.cross_val_score(logreg, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
 print "Logistic Regression Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
 plotting(5, logreg, "Logistic Regression")
@@ -98,10 +84,11 @@ plotting(5, logreg, "Logistic Regression")
 
 dtree = tree.DecisionTreeClassifier()
 dtree.fit(train[:,0:2], label)
+print "Decision tree test data accuracy: %.2f%%" % (dtree.score(test[:,0:2], np.reshape(test[:,2:3], test[:,2:3].shape[0]))*100)
 scores = cross_validation.cross_val_score(dtree, csv[:,0:2], np.reshape(csv[:,2:3], csv[:,2:3].shape[0]), cv=5)
 print "Decision Tree Cross-Validation accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
 plotting(6, dtree, "Decision Tree")
 
 
 plt.show()
-	
+
