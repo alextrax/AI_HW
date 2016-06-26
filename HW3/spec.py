e1e75661cfc4763d70af1e1cd293e7c56822c060
alextrax <cw2952@columbia.edu>
2016-06-26 00:19:16 -0400

    Fix image plotting

--- HW3/spec.py
+++ HW3/spec.py
@@ -12,9 +12,9 @@ import matplotlib as mpl
 ncolors = 4
 
 
-trees = misc.imread("linkedin.png")
-trees = np.array(trees, dtype=np.float64)
-trees = misc.imresize(trees, 0.8) / 255.
+trees = misc.imread("usa.png")
+trees = np.array(trees, dtype=np.float64) / 255
+#trees = misc.imresize(trees, 0.8) / 255.
 print trees.shape
 
 w, h, d = tuple(trees.shape)
@@ -23,6 +23,7 @@ trees_2d = np.reshape(trees, (w * h, d))
 
 quantized = SpectralClustering(n_clusters=ncolors, eigen_solver='arpack').fit_predict(trees_2d)
 
+'''
 cmap = mpl.cm.cool
 norm = mpl.colors.Normalize(vmin=0, vmax=ncolors)
 print quantized.shape#
@@ -30,4 +31,33 @@ print quantized.shape#
 image = np.reshape(quantized, (w , h))
 
 plt.imshow(image, vmin=0, vmax=ncolors)    
+'''
+
+total = {}
+count = {}
+index = 0
+for i in range(w):  # calculate cluster center RGB value based on spectral label
+    for j in range(h):
+        if quantized[index] not in total:
+            total[quantized[index]] = trees[i,j]
+            count[quantized[index]] = 1
+        else:    
+            total[quantized[index]] += trees[i,j]
+            count[quantized[index]] += 1
+        index += 1
+
+for l in total:
+    total[l] = total[l]/count[l]
+
+# Plotting image based on cluster center value
+plt.figure(2)
+image = np.zeros((w, h, d))
+index = 0
+for i in range(w):
+    for j in range(h):
+        image[i][j] = total[quantized[index]]
+        index += 1
+
+plt.imshow(image)    
 plt.show()
+
