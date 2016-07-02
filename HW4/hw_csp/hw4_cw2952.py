c5fd7c481a60d61585db227055e81c6cda4cf4b2
alextrax <cw2952@columbia.edu>
2016-07-02 19:58:58 -0400

    HW4 final

--- HW4/hw_csp/hw4_cw2952.py
+++ HW4/hw_csp/hw4_cw2952.py
@@ -2,6 +2,7 @@
 #coding:utf-8
 
 import string
+from copy import deepcopy
 
 ROW = "ABCDEFGHI";
 COL = "123456789";
@@ -14,6 +15,12 @@ def printSudoku(sudoku):
 			print sudoku[i + j],
 		print ""	
 
+def outputSudoku(sudoku, f):
+	f.write("-----------------\n")
+	for i in ROW:
+		for j in COL:
+			f.write(str(sudoku[i + j])+" "),
+		f.write("\n")
 
 def get_square_checklist(var):
 	if var[0] == 'A' or var[0] == 'B' or var[0] == 'C':
@@ -54,15 +61,15 @@ def get_arcs(var, sudo): # return var->others and others->var
 	allcol = [var[0]+str(x) for x in range(1,10)]
 	allrow = [x+var[1] for x in string.uppercase[:9]]
 	for i in square:
-		if i != var: #sudo[i] == 0:
+		if i != var and sudo[i] == 0:
 			candidate.append(i)
 
 	for i in allcol:
-		if i not in candidate and i != var:
+		if i not in candidate and i != var and sudo[i] == 0:
 			candidate.append(i)
 
 	for i in allrow:
-		if i not in candidate and i != var:
+		if i not in candidate and i != var and sudo[i] == 0:
 			candidate.append(i)
 	
 	return [(var, x) for x in candidate], [(x, var) for x in candidate]		
@@ -71,13 +78,10 @@ def get_arcs(var, sudo): # return var->others and others->var
 def remove_inconsistant(i, j, d_avail):
 	removed = False
 	avail_list = list(d_avail[i])[::]
-	#print i,j,avail_set
 	for x in avail_list:
 		if len(d_avail[j]) == 1 and list(d_avail[j])[0] == x:
 			removed = True
-			#print i, avail_list, j, d_avail[j]
 			avail_list.remove(x)
-			#print "remove", x
 	d_avail[i] = set(avail_list)
 	return removed
 
@@ -93,14 +97,6 @@ def ac3(sudo):
 			queue += get_arcs(i, sudo)[1]
 			d_avail[i] = get_availale(i, sudo)	
 
-	'''
-	for i in d_avail:
-		if len(d_avail[i]) > 1:
-			sudo[i] = d_avail[i].pop()
-			d_avail[i] = set([sudo[i]])
-			break
-	'''
-
 	while(len(queue) > 0):
 		i,j = queue.pop(0)
 
@@ -112,6 +108,72 @@ def ac3(sudo):
 			return False
 	return True	
 
+def getMRV(candlist, d_avail):
+	#print candlist
+	tmp = sorted(candlist, key=lambda x: len(d_avail[x]))
+	return tmp[0]
+
+def update_avail(d_avail, sudo): 
+	for i in sudo:
+		if sudo[i] == 0:
+			d_avail[i] = get_availale(i, sudo)
+
+def recursive_backtracking(sudo, candlist, d_avail):
+	if len(candlist) == 0: # All variables assigned
+		return sudo
+	variable = getMRV(candlist, d_avail)
+	avails = get_availale(variable, sudo)
+	for value in avails:
+		new_sudo = deepcopy(sudo)
+		new_sudo[variable] = value
+		new_candlist = candlist[::]
+		new_candlist.remove(variable) 
+		new_d_avail = deepcopy(d_avail)
+		update_avail(new_d_avail, sudo)  # Forward checking
+		del new_d_avail[variable]
+		
+		result = recursive_backtracking(new_sudo, new_candlist, new_d_avail)
+		if result != None:
+			return result
+	return None		
+
+def backtracking(sudo):
+	d_avail = {}
+	unassigned = []
+	for i in sudo:
+		if sudo[i] == 0:
+			d_avail[i] = get_availale(i, sudo)
+			unassigned.append(i)
+
+	return recursive_backtracking(sudo, unassigned, d_avail)
+
+
+def evaluate(var, sudo):
+	available = set(range(1,10))
+	square = get_square_checklist(var)
+	allcol = [var[0]+str(x) for x in range(1,10)]
+	allrow = [x+var[1] for x in string.uppercase[:9]]
+	for i in square:
+		if i != var:
+			available.discard(sudo[i])
+
+	for i in allcol:
+		if i != var:
+			available.discard(sudo[i])
+
+	for i in allrow:
+		if i != var:
+			available.discard(sudo[i])		
+
+	return available
+
+def evaluate_sudoku(sudo):
+	for i in sudo:
+		if len(evaluate(i, sudo)) != 1:
+			return False
+	return True	
+
+
 # Reading of sudoku list from file
 try:
     f = open("sudokus.txt", "r")
@@ -122,21 +184,35 @@ except:
 
 # 1.5 count number of sudokus solved by AC-3
 num_ac3_solved = 0
+solvable = []
+i = 0
 for line in sudokuList.split("\n"):
 	# Parse sudokuList to individual sudoku in dict, e.g. sudoku["A2"] = 1
 	sudoku = {ROW[i] + COL[j]: int(line[9*i+j]) for i in range(9) for j in range(9)}
+	i += 1
 	if ac3(sudoku) == True:
 		num_ac3_solved += 1
+		solvable.append(i)
+		
 
-print "num_ac3_solved:",num_ac3_solved	
+print "num_ac3_solved:",num_ac3_solved, solvable	
 
 	# write your AC3 algorithms here, update num_ac3_solved	
 
 # 1.6 solve all sudokus by backtracking
-#for line in sudokuList.split("\n"):
+success = 0
+fail = 0
+f = open('output_cw2952.txt','w')
+for line in sudokuList.split("\n"):
 	# Parse sudokuList to individual sudoku in dict, e.g. sudoku["A2"] = 1
-	#sudoku = {ROW[i] + COL[j]: int(line[9*i+j]) for i in range(9) for j in range(9)}
+	sudoku = {ROW[i] + COL[j]: int(line[9*i+j]) for i in range(9) for j in range(9)}
 
 	# write your backtracking algorithms here
-
-	#printSudoku(sudoku)
+	sudoku = backtracking(sudoku)
+	if evaluate_sudoku(sudoku) == True:
+		success += 1
+	else:
+		fail += 1	
+	printSudoku(sudoku)
+	outputSudoku(sudoku, f)
+print "Success:",success,", Fail:", fail	
